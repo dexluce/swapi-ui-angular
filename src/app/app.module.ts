06133c70dfea5b26efeaa5599ae6f81b9a404c6a
balthazar <balthazar.de.moncuit@gmail.com>
2020-02-28 17:10:37 +0100

    move all components in "components" folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { ListComponent } from './list/list.component';
-import { ListItemComponent } from './list-item/list-item.component';
-import { SearchFormComponent } from './search-form/search-form.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ListComponent } from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 @NgModule({
   declarations: [
