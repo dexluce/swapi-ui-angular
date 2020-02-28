01ee0080f50800cb9b0b85efc6a3ba6806ebff45
balthazar <balthazar.de.moncuit@gmail.com>
2020-02-28 17:14:41 +0100

    move app component in "components" folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
