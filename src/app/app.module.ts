5dc09cfc8bc8c055aebf09990bcf5d3bbd262ef2
balthazar <balthazar.de.moncuit@gmail.com>
2020-02-28 15:30:46 +0100

    add generated components needed for swapi-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    LoginFormComponent,
+    SearchBarComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
