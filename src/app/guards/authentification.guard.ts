cde74a356396b858bac43efce1b7d20ff66d3743
balthazar <balthazar.de.moncuit@gmail.com>
2020-03-02 20:55:45 +0100

    emplemente auth guard

--- src/app/guards/authentification.guard.ts
+++ src/app/guards/authentification.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.userService.isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;  }
+    }
 }
