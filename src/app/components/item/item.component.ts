fbf6db610452c6bc850f3969415af7a84335efad
balthazar <balthazar.de.moncuit@gmail.com>
2020-03-05 00:46:05 +0100

    implement item

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/swapi.state';
+import { Item } from 'src/app/models';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnDestroy {
+  url: string;
+  item: Observable<Item>;
+  private subscribtionToUrlParam: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private store: Store<AppState>) { }
 
-  ngOnInit(): void {
-    
+  ngOnInit() {
+    this.subscribtionToUrlParam = this.route.params.subscribe((params) => {
+       this.url = params['itemUrl'];
+       this.item = this.store.select(state => state.app.searchResult.find(item => item.url === this.url))
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscribtionToUrlParam.unsubscribe();
   }
 
 }
