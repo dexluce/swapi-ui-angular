ca6c6ad57f31ea5fc32cf04065f19d948ed653fd
balthazar <balthazar.de.moncuit@gmail.com>
2020-03-08 23:33:55 +0100

    add people component swapi is out of service while coding this line. May be buggy

--- src/app/components/items/people/people.component.ts
+++ src/app/components/items/people/people.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { People } from 'src/app/models';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
   styleUrls: ['./people.component.css']
 })
-export class PeopleComponent implements OnInit {
-
+export class PeopleComponent {
+  @Input() people: People;
+  
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
