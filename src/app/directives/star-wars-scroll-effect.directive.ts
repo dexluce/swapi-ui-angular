292b5a560147aad6a8cac9bd05692e18c1eb3cee
balthazar <balthazar.de.moncuit@gmail.com>
2020-03-09 00:10:39 +0100

    add last star wars directive update This directive will be destroy soon, but let's put what we got...

--- src/app/directives/star-wars-scroll-effect.directive.ts
+++ src/app/directives/star-wars-scroll-effect.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef, HostListener, Input, Renderer2, AfterViewInit } from '@angular/core';
 import { style, animate, AnimationPlayer, AnimationBuilder, AnimationMetadata } from '@angular/animations';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Directive({
   selector: '[appStarWarsScrollEffect]'
@@ -12,7 +13,7 @@ export class StarWarsScrollEffectDirective implements AfterViewInit {
   private isPlaying = true; // While we are player the animation we can't allow the user to scroll. This because angular doesn't implement player.setPosition / player.getPosition yet
   private animation: AnimationMetadata[] = [
     style({ top: '95%' }),
-    animate('8s linear', style({ top: 0 })),
+    animate('1s linear', style({ top: 0 }))
   ];
 
   @HostListener("window:wheel", ["$event"]) onScroll(e: WheelEvent) {
@@ -31,28 +32,41 @@ export class StarWarsScrollEffectDirective implements AfterViewInit {
     private el: ElementRef,
     private renderer: Renderer2,
     private builder: AnimationBuilder,
-    private snackBarService: MatSnackBar
+    private snackBarService: MatSnackBar,
+    private router: Router
   ){ }
   
   ngAfterViewInit() {
-    this.snackBarService.open("wait ...", null, { duration: 1000 })
-
+    console.log("init")
     const parent = this.el.nativeElement.parentNode;
     const div = this.renderer.createElement("div");
-    
     this.renderer.insertBefore(parent, div, this.el.nativeElement);
     this.renderer.removeChild(parent, this.el.nativeElement);
     this.renderer.appendChild(div, this.el.nativeElement)
     
-    this.renderer.setStyle(div, "height", "100%");
-    this.renderer.setStyle(div, "overflow", "hidden");
-    this.renderer.setStyle(div, "marginTop", "-250px");
     this.renderer.setStyle(div, "transform", "perspective(100px) rotateX(25deg)");
+    this.renderer.setStyle(div, "overflow", "hidden");
+
     this.renderer.setStyle(this.el.nativeElement, "position", "absolute");
     this.renderer.setStyle(this.el.nativeElement, "left", "35%");
     this.renderer.setStyle(this.el.nativeElement, "right", "35%");
-    
-    this.topLimite = -(100 / div.offsetHeight * this.el.nativeElement.offsetHeight) 
+
+
+    this.renderer.setStyle(div, "height", "100%");
+    this.renderer.setStyle(div, "marginTop", "-250px");
+
+    this.viewInit();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewInit();
+      }
+    });
+  }
+
+  private viewInit() {
+    this.top = 0;
+    this.isPlaying = true;
+    this.topLimite = -(100 / this.el.nativeElement.offsetHeight * this.el.nativeElement.children.offsetHeight) 
 
     const factory = this.builder.build(this.animation);
     this.player = factory.create(this.el.nativeElement);
